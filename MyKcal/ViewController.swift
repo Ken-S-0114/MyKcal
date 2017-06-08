46753b200123ea510954c2b841ffa87acc247427
Ken-S-0114 <satoken@satoukennoMacBook-Pro.local>
2017-06-08 22:33:28 +0900

    Initial Commit

---
+++ MyKcal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyKcal
+//
+//  Created by 佐藤賢 on 2017/06/08.
+//  Copyright © 2017年 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
